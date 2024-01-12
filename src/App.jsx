5822ac165dc2daf32864918b470c9e5b80aa47a0
Bhekisile <bhekisilejozi@gmail.com>
2024-01-12 16:18:48 +0200

    Implementing routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 
 const router = createBrowserRouter([
   {
+    element: <AppLayout />,
+    children: [
+      {
     path: '/',
     element: <Home />,
   },
@@ -18,7 +22,9 @@ const router = createBrowserRouter([
   { path: '/cart', element: <Cart /> },
   { path: '/order/new', element: <CreateOrder /> },
   { path: '/order/:orderId', element: <Order /> },
-])
+    ]
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,55 @@
+function Header() {
+  return (
+    <header>
+      <link to='/'>Fast React Pizza Co.</link>
+
+      <p>Bhekisile</p>
+    </header>
+  )
+}
+
+export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
