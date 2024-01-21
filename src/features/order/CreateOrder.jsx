d1ba9912c49e356ca68c21f1e0b08603d0545b2e
Bhekisile <bhekisilejozi@gmail.com>
2024-01-21 14:10:24 +0200

    Style buttons

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -85,7 +85,12 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button disabled={isSubmitting}
+          className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase 
+          tracking-wide text-stone-800 hover:bg-yellow-300 transition-colors duration-300
+          focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2
+          disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing Order..." : "Order now"}
           </button>
         </div>
