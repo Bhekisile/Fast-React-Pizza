293d9e1d8cb83b83cfb21a14439a529842cd379a
Bhekisile <bhekisilejozi@gmail.com>
2024-02-06 17:52:54 +0200

    Delete cart items

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,6 +1,7 @@
 import { formatCurrency } from "../../utils/helpers";
 import PropTypes from 'prop-types';
-import Button from "../../ui/Button";
+// import Button from "../../ui/Button";
+import DeleteItem from "./DeleteItem";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
@@ -14,7 +15,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <DeleteItem pizzaId={pizzaId}/>
       </div>
     </li>
   );
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,15 @@
 import { formatCurrency } from "../../utils/helpers";
 import PropTypes from 'prop-types';
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     // console.log(id);
@@ -29,7 +32,8 @@ function MenuItem({ pizza }) {
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>}
           
-          { !soldOut && ( <Button type='small' onClick={handleAddToCart} >Add to Cart</Button> ) }
+          {isInCart && <DeleteItem pizzaId={id} />}
+          { !soldOut && !isInCart && ( <Button type='small' onClick={handleAddToCart} >Add to Cart</Button> ) }
         </div>
       </div>
     </li>
