5eccd833913d545018b7fe0876604acac32da1ad
Bhekisile <bhekisilejozi@gmail.com>
2024-02-09 11:27:13 +0200

    Fetch data using menu router

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,13 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+  const fetcher = useFetcher();
+
+  useEffect(function () {
+    if (!fetcher.data && fetcher.state === 'idle')
+    fetcher.load('/menu');
+  },
+  [fetcher]
+  );
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -42,7 +52,12 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            key={item.pizzaId}
+            ingredients={fetcher?.data?.find((el) => el.id === item.pizzaId)?.ingredients ?? []}
+          />
         ))}
       </ul>
 
